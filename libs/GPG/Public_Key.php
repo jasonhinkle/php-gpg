11835c38d70fa7fb8d1b83a7a028598cf9d177ec
Daniel Ruf <DanielRuf@users.noreply.github.com>
2016-01-19 08:59:37 +0100

    fixed E_NOTICE caused by specific public keys

--- libs/GPG/Public_Key.php
+++ libs/GPG/Public_Key.php
@@ -137,8 +137,25 @@ class GPG_Public_Key {
                 
 				if ($len > 191 && $len < 224)
                     $len = (($len - 192) << 8) + ord($sa[$i++]) + 192; // there was a bug here - fixed with RFC4880 4.2.2.2.
-				else if ($len == 255)
-                    $len = (ord($sa[$i++]) << 24) + (ord($sa[$i++]) << 16) + (ord($sa[$i++]) << 8) + ord($sa[$i++]);
+				else if ($len == 255){
+				$oc1=isset($sa[++$i])?$sa[$i]:NULL;
+				$oc2=isset($sa[++$i])?$sa[$i]:NULL;
+				$oc3=isset($sa[++$i])?$sa[$i]:NULL;
+				$oc4=isset($sa[++$i])?$sa[$i]:NULL;
+
+				if($oc1!==NULL)$len = ord($oc1)* pow(2,24);
+				else $len = (0* pow(2,24));
+
+				if($oc2!==NULL)$len += ord($oc2)* pow(2,16);
+				else $len += (0* pow(2,16));
+
+				if($oc3!==NULL)$len += ord($oc3)* pow(2,8);
+				else $len += (0* pow(2,8));
+
+				if($oc4!==NULL)$len += ord($oc4);
+				else $len += (0);
+				}
+                // else if ($len == 255) $len = (ord($sa[$i++]) << 24) + (ord($sa[$i++]) << 16) + (ord($sa[$i++]) << 8) + ord($sa[$i++]);
                 else if ($len > 223 && $len < 255)
                     $len = (1 << ($len & 0x1f));
                 // else, length is less or equal 191, so no transform is needed.
